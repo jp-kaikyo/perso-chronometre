0bedf5e03c1e0c9505e7d833e25bc771346f2c50
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-09-10 02:30:24 +0200

    mise a jours du js

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 
 
 // temps //
-
 let h = 0;
 let m = 0;
 let s = 0;
 
-
 // btn //
-
 const start = true;
 const stop = "null";
 const reset = false;
 
 
 
-//// RECUP DES INFO HTML ////
+//// Attendre que HTML et CSS soient chargés ////
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page chargée !');
+});
